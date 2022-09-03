2ee540896b8d39b5484f61be00740d7ab246bf35
Christian-Mogensen <u2407192@rts-365.dk>
2022-09-03 11:24:42 +0200

    Add styling, dummy data & routing to dynamic pages - temp solution

--- app-loesning/pages/schedule.tsx
+++ app-loesning/pages/schedule.tsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { AiFillStar } from "react-icons/ai";
@@ -16,19 +17,87 @@ export async function getStaticProps() {
     },
   };
 }
-
-const Schedule: NextPage = ({ assets }: any) => {
-  const router = useRouter();
-  const { pid } = router.query;
-  console.log(pid);
+const userClass: any = {
+  classes: [
+    {
+      className: "Yoga",
+      classDay: "Monday",
+      classTime: "10:00",
+      Roster: {
+        classId: 1,
+      },
+    },
+    {
+      className: "Pump it up",
+      classDay: "Tuesday",
+      classTime: "17:00",
+      Roster: {
+        classId: 2,
+      },
+    },
+    {
+      className: "Yoga",
+      classDay: "Wednesday",
+      classTime: "15:00",
+      Roster: {
+        classId: 3,
+      },
+    },
+    {
+      className: "Pump it up",
+      classDay: "Friday",
+      classTime: "17:30",
+      Roster: {
+        classId: 4,
+      },
+    },
+    {
+      className: "Get fit or die trying, whatever you want and more text",
+      classDay: "Friday",
+      classTime: "17:30",
+      Roster: {
+        classId: 5,
+      },
+    },
+  ],
+};
+const Schedule: NextPage = ({}: any) => {
+  const [data, setData] = useState<any>(userClass.classes);
+  //   useEffect(() => {
+  //     setData(userClass.classes);
+  //   }, []);
   return (
     <>
       <Head>
         <title>Svendeprøve - overview</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="w-screen min-h-screen p-5 mt-20 overflow-x-hidden"></div>
+      <div className="w-screen min-h-screen p-5 mt-20 overflow-x-hidden">
+        <ul>
+          {data.map((item: any, i: number) => (
+            <ClassItem key={i}>
+              <Link href={`/class/${item.Roster.classId}`}>
+                <a>
+                  <h3 className="text-xl font-semibold truncate">
+                    {item.className}
+                  </h3>
+                  <h4 className="mt-3">
+                    {item.classDay} - {item.classTime}
+                  </h4>
+                </a>
+              </Link>
+            </ClassItem>
+          ))}
+        </ul>
+      </div>
     </>
+  );
+};
+const ClassItem = ({ children }: any) => {
+  return (
+    <li className="p-3 px-5 mt-3 text-black border first-of-type:mt-0 bg-ashe-light border-ashe-dark rounded-2xl">
+      {children}
+    </li>
   );
 };
 
